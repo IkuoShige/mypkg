20d2ed3bb3c970ff04233e81f4ac4dfa97866976
IkuoShige <shigeikuo@gmail.com>
2022-12-31 23:02:06 +0900

    add license

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2022 Ikuo Shige                                                                                                           
+# SPDX-License-Identifier: BSD-3-Clause
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16#lesson8
