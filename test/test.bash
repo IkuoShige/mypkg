4c82da7731e83f4c5585c3db092d9b65560a52f7
IkuoShige <shigeikuo@gmail.com>
2023-01-03 18:25:00 +0900

    update test

--- test/test.bash
+++ test/test.bash
@@ -16,12 +16,14 @@ source $dir/.bashrc
 cd src/mypkg/test/
 echo "read" > "option.txt"
 ls option.txt
+ros2 run mypkg hit_and_blow < 'input.txt' > '/tmp/mypkg.log' &
+ros2 run mypkg hit_and_blow_B < 'input.txt'
 #gnome-terminal -- bash -c "bash run_1.bash"
-gnome-terminal -- bash -c "ros2 run mypkg hit_and_blow_B < 'input.txt' > '/tmp/mypkg.log'"
+#gnome-terminal -- bash -c "ros2 run mypkg hit_and_blow_B < 'input.txt' > '/tmp/mypkg.log'"
 #ros2 run mypkg hit_and_blow_B < 'input.txt' > '/tmp/mypkg.log'
-sleep 1
+#sleep 1
 #ros2 run mypkg hit_and_blow < 'input.txt'
-gnome-terminal -- bash -c "ros2 run mypkg hit_and_blow < 'input.txt'"
+#gnome-terminal -- bash -c "ros2 run mypkg hit_and_blow < 'input.txt'"
 #gnome-terminal -- bash -c 'bash run_2.bash'
 #rm "option.txt"
 #cat option.txt
